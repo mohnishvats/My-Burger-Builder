1175d6959eff24caae952b446eb7caa87625eb44
mohnish <mohnishvats@gmail.com>
2021-01-12 19:46:39 +0530

    i added state in burger builder for setting burger ingredients dynamically

--- src/Containers/BurgerBuilder/burgerbuilder.js
+++ src/Containers/BurgerBuilder/burgerbuilder.js
@@ -3,10 +3,18 @@ import Aux from "../../hoc/Aux";
 import Burger from "../../Components/Burger/Burger";
 
 class burgerbuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      meat: 0,
+      bacon: 0,
+      cheese: 0,
+    },
+  };
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>build controls</div>
       </Aux>
     );
