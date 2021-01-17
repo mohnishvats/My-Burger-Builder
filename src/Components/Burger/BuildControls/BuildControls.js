5105bf9ebbc9156be5114f7fd8c6724b91c6007f
mohnish <mohnishvats@gmail.com>
2021-01-17 20:56:18 +0530

    i add build controls to this burger app

---
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classes from "./BuildControl/BuildControl.css";
+import "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
+
+const buildControls = (props) => (
+  <div className="BuildControls">
+    <p>current price: {props.price}</p>
+    {controls.map((ctrl) => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.type}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+    <button>ORDER NOW</button>
+  </div>
+);
+
+export default buildControls;
